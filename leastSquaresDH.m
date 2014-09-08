2b18c0726a69077912698f1d491dd53492f66dfe
Eamon Taaffe <eamontaaffe@gmail.com>
2014-09-08 17:03:17 +1000

    leastSquares updated a little

--- leastSquaresDH.m
+++ leastSquaresDH.m
@@ -1,6 +1,6 @@
 clc
 clear all
-syms a1 a2 a5 d1 d3 d4 d5 d6 t1 t2 t3 t4 t5 t6
+syms a1 a2 a5 d1 d3 d4 d5 d6 t1 t2 t3 t4 t5 t6 x y z
 equation = ...
 [[d3*(cos(t1)*cos(t2) - sin(t1)*sin(t2)) - d6*(sin(t5)*(cos(t3)*sin(t4)*(cos(t1)*sin(t2) + cos(t2)*sin(t1)) + cos(t4)*sin(t3)*(cos(t1)*sin(t2) + cos(t2)*sin(t1))) - cos(t5)*(cos(t1)*cos(t2) - sin(t1)*sin(t2))) + d4*(cos(t1)*cos(t2) - sin(t1)*sin(t2)) - d5*(cos(t3)*cos(t4)*(cos(t1)*sin(t2) + cos(t2)*sin(t1)) - sin(t3)*sin(t4)*(cos(t1)*sin(t2) + cos(t2)*sin(t1))) + a2*sin(t3)*(cos(t1)*sin(t2) + cos(t2)*sin(t1)) + a5*sin(t6)*(cos(t3)*cos(t4)*(cos(t1)*sin(t2) + cos(t2)*sin(t1)) - sin(t3)*sin(t4)*(cos(t1)*sin(t2) + cos(t2)*sin(t1))) - a1*cos(t1)*sin(t2) - a1*cos(t2)*sin(t1) + a5*cos(t6)*(cos(t5)*(cos(t3)*sin(t4)*(cos(t1)*sin(t2) + cos(t2)*sin(t1)) + cos(t4)*sin(t3)*(cos(t1)*sin(t2) + cos(t2)*sin(t1))) + sin(t5)*(cos(t1)*cos(t2) - sin(t1)*sin(t2)))]
 [d6*(sin(t5)*(cos(t3)*sin(t4)*(cos(t1)*cos(t2) - sin(t1)*sin(t2)) + cos(t4)*sin(t3)*(cos(t1)*cos(t2) - sin(t1)*sin(t2))) + cos(t5)*(cos(t1)*sin(t2) + cos(t2)*sin(t1))) + d3*(cos(t1)*sin(t2) + cos(t2)*sin(t1)) + d4*(cos(t1)*sin(t2) + cos(t2)*sin(t1)) + d5*(cos(t3)*cos(t4)*(cos(t1)*cos(t2) - sin(t1)*sin(t2)) - sin(t3)*sin(t4)*(cos(t1)*cos(t2) - sin(t1)*sin(t2))) - a2*sin(t3)*(cos(t1)*cos(t2) - sin(t1)*sin(t2)) + a1*cos(t1)*cos(t2) - a5*sin(t6)*(cos(t3)*cos(t4)*(cos(t1)*cos(t2) - sin(t1)*sin(t2)) - sin(t3)*sin(t4)*(cos(t1)*cos(t2) - sin(t1)*sin(t2))) - a1*sin(t1)*sin(t2) - a5*cos(t6)*(cos(t5)*(cos(t3)*sin(t4)*(cos(t1)*cos(t2) - sin(t1)*sin(t2)) + cos(t4)*sin(t3)*(cos(t1)*cos(t2) - sin(t1)*sin(t2))) - sin(t5)*(cos(t1)*sin(t2) + cos(t2)*sin(t1)))]
@@ -15,3 +15,11 @@ for i = 1:length(equation)
         A{i,j} = a(length(a));
     end
 end
+disp('The X matrix is:')
+X = [x;y;z];
+pretty(X)
+disp('The A matrix is:')
+A = [A{1,:};A{2,:};A{3,:}];
+pretty(simplify(A))
+disp('The B matrix is:')
+pretty(B)
